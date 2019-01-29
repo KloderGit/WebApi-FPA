3cd12322dacbeb2a8491ca21e46875ce7c26a62f
Илья Иджян <kloder3@gmail.com>
2019-01-29 14:57:50 +0300

    add ViewModel add CallBackController

---
+++ Controllers/CallBack/CallBackController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApi.Controllers.CallBack.Models;
+
+namespace WebApi.Controllers.CallBack
+{
+    [Produces("application/json")]
+    [Route("CallBack")]
+    public class CallBackController : Controller
+    {
+        ILogger logger;
+
+        public CallBackController(ILoggerFactory loggerFactory)
+        {
+            this.logger = loggerFactory.CreateLogger(this.ToString());
+        }
+
+
+        [HttpPost]
+        [Route("request")]
+        public async Task<IActionResult> GetModel([FromBody] CallBackViewModel model)
+        {
+            logger.LogError("Получена форма на запрос Перезвонить {@Model}", model);
+
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/CallBack/Models/CallBackViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers.CallBack.Models
+{
+    public class CallBackViewModel
+    {
+        public string UserName { get; set; }
+        public string UserPhone { get; set; }
+        public string ProgramType { get; set; }
+        public string ProgramTitle { get; set; }
+        public string Url { get; set; }
+    }
+}
