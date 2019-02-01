90d3b1bf374d42f6b0d9ab539b50d89d38a7957e
Илья Иджян <kloder3@gmail.com>
2019-02-01 13:11:21 +0300

    Отправка данных в логику возврат результата

--- Controllers/CallBack/CallBackController.cs
+++ Controllers/CallBack/CallBackController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryAmoCRM.Interfaces;
+using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApi.Controllers.CallBack.Models;
+using WebApiBusinessLogic.Logics.CallBack;
+using WebApiBusinessLogic.Logics.CallBack.Models;
 
 namespace WebApi.Controllers.CallBack
 {
@@ -13,11 +17,19 @@ namespace WebApi.Controllers.CallBack
     [Route("CallBack")]
     public class CallBackController : Controller
     {
+        TypeAdapterConfig mapper;
         ILogger logger;
+        IDataManager crm;
 
-        public CallBackController(ILoggerFactory loggerFactory)
+        CallBackLogic logic;
+
+        public CallBackController(TypeAdapterConfig mapper, IDataManager crm, ILoggerFactory loggerFactory)
         {
+            this.mapper = mapper;
+            this.crm = crm;
             this.logger = loggerFactory.CreateLogger(this.ToString());
+
+            this.logic = new CallBackLogic(mapper, crm, loggerFactory);
         }
 
 
@@ -26,6 +38,10 @@ namespace WebApi.Controllers.CallBack
         public async Task<IActionResult> GetModel([FromBody] CallBackViewModel model)
         {
             logger.LogError("Получена форма на запрос Перезвонить {@Model}", model);
+
+            var res = await logic.CreateRecallTask(model.Adapt<CallBackDTO>());
+
+            if (!res) return StatusCode(500);
 
             return Ok();
         }
