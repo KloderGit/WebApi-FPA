590c72ec3d9ef03e8e11624aed3888ad0e292ef4
Илья Иджян <kloder3@gmail.com>
2019-02-13 15:20:40 +0300

    add Logger

--- Controllers/Listener/ListenerController.cs
+++ Controllers/Listener/ListenerController.cs
@@ -1,6 +1,7 @@
 ﻿using Common.Mapping;
 using Mapster;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using WebApi.Controllers.Listener.LocalMaps;
@@ -15,14 +16,17 @@ namespace WebApi.Controllers.Listener
     public class ListenerController : Controller
     {
         TypeAdapterConfig mapper;
+        ILogger logger;
 
-        LIstenerLogic logic = new LIstenerLogic();
+        ListenerLogic logic = new ListenerLogic();
 
-        public ListenerController(TypeAdapterConfig mapper)
+        public ListenerController(TypeAdapterConfig mapper, ILoggerFactory loggerFactory)
         {
             this.mapper = mapper;
             new RegisterCommonMaps(mapper);
             new IncomingEventToDTO(mapper);
+
+            this.logger = loggerFactory.CreateLogger(this.ToString());
         }
 
         [HttpPost]
@@ -30,7 +34,9 @@ namespace WebApi.Controllers.Listener
         {
             var model = value.Adapt<IEnumerable<EventDTO>>(mapper);
 
+            logger.LogInformation("LISTENER2 - Событие AmoCRM {@value} | DTO {@model}", value.First(), model.First());
 
+            logic.EventsHandle(model);
 
 
             return Ok(model);
